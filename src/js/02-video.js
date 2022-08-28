f79cb73ce8fb3f4b44c0c363b46a09b3cb77055e
Elena-Sea <smetyukelena@gmail.com>
2022-08-28 23:10:58 +0200

    adds code to task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+onVideoRestart();
+
+function onPlay(data) {
+    const savedPlayedTime = data.seconds;
+    localStorage.setItem(STORAGE_TIME, savedPlayedTime);
+}
+
+function onVideoRestart() { 
+    const lastTimeUpdate = localStorage.getItem(STORAGE_TIME);
+    if (lastTimeUpdate) { 
+        player.setCurrentTime(lastTimeUpdate);
+    };
+}
