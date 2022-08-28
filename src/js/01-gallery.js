b6ba5a057e91fac03d92a0d80c660184c19dba31
Elena-Sea <smetyukelena@gmail.com>
2022-08-28 20:51:42 +0200

    adds task 1 (gallery.js)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = creatGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend',galleryMarkup);
+
+function creatGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+     }).join('');
+};
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250
+    });
+    
+gallery.on('show.simplelightbox', function () {});
